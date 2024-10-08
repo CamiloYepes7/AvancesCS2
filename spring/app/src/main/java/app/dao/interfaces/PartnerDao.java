77dfa69793f86d4e662950a2b9ce82227f419f9e
CamiloYepes7 <camiloyepes0703@gmail.com>
2024-10-08 16:52:12 -0500

    modify proyect

--- spring/app/src/main/java/app/dao/interfaces/PartnerDao.java
+++
@@ -1,9 +0,0 @@
-package app.dao.interfaces;
-
-import app.dto.PersonDto;
-
-public interface PartnerDao {
-        public void createGuest(PersonDto personDto) throws Exception;
-        public void disableGuest(PersonDto personDto)throws Exception;
-        public void enableGuest(PersonDto personDto)throws Exception;
-}
